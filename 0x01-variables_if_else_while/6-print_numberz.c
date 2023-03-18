338e4cb81ee85471575251dd1e80425f5afa47d1
akosua88 <asantewaaa476@gmail.com>
2023-03-18 06:10:30 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints all single digit numbers of base
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	putchar(d);
+	putchar('\n')
+	return (0);
+}
