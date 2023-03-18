ce89096654e98e6cfbaa04e56786927da2894ff3
akosua88 <asantewaaa476@gmail.com>
2023-03-18 05:24:57 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A  program that prints all single digit numbers of base
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	printf("%d", c);
+	printf('\n');
+	return (0);
+}
