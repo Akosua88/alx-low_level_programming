78376b9bca4377d622ff1cf88929a099d19e6155
akosua88 <asantewaaa476@gmail.com>
2023-03-21 14:08:20 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet, in lowercase
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
