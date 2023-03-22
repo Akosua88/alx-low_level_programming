bbe4160f4c79f975c8e85e0449b36d29657005ee
akosua88 <asantewaaa476@gmail.com>
2023-03-21 19:20:12 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,14 +2,15 @@
 
 /**
  * main - Prints the sum of the even-valued terms
- * Return: Always (success)
+ * less than 4000000
+ * Return: Nothing!
  */
 
 int main(void)
 
 {
 
-	int i = 0
+	int i = 0;
 	long j = 1, k = 2, sum = k;
 
 	while (k + j < 4000000)
